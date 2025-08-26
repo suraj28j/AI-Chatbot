e91642b9de467442cd223b45089a64bf653968b8
Suraj Kumar <suraj28j@gmail.com>
2025-08-26 17:06:49 +0530

    UI complete

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,18 @@ import './App.css'
 function App() {
   return (
     <>
-     <h2 className='bg-green-200'>Welcome</h2>
+      <div className='grid grid-cols-12 h-screen text-center'>
+        <div className='col-span-2 bg-zinc-800'>Recent Searches</div>
+        <div className='col-span-10 bg-zinc-900'>
+          <div className='h-110 p-10'>
+            <h1>Hello user ask me anything</h1>
+          </div>
+          <div className='bg-zinc-800 w-1/2 text-white m-auto border border-zinc-400 rounded-4xl p-1 pr-5 flex'>
+            <input type='text' className='w-full h-full p-3 outline-none' placeholder='Ask me anything' />
+            <button>Ask</button>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
